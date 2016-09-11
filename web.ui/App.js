df115951ece413c2b1be975ce44a195a31e517a6
rahardiandj <rahardiandj@gmail.com>
2016-09-11 09:03:24 +0800

    Modified react js project

--- web.ui/App.js
+++ web.ui/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
+
+
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      height:100
+    }
+  }
+
     render(){
-        return <div>Hello</div>
+        var {name, age, pict} = this.props;
+        var {height} = this.state;
+        return (
+          <div>
+          <h1>{name}</h1>
+          <h3>{age}</h3>
+          <img src={pict} height={height}  />
+          <br />
+          <button onClick={this.zoomPicIn.bind(this)}>+</button>
+          <button onClick={this.zoomPicOut.bind(this)}>-</button>
+          </div>
+        )
+    }
+    zoomPicIn(){
+      this.setState({height:this.state.height + 30});
+    }
+    zoomPicOut(){
+      this.setState({height:this.state.height - 30});
     }
 }
 
--- web.ui/main.js
+++ web.ui/main.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App name="Juli" age={33} pict="https://www.askideas.com/media/19/Child-Making-Funny-Face.jpg" height={100}/>, document.getElementById('app'))
