2b12aa838d07df09d6f979631b5089e43c1981c3
rahardiandj <rahardiandj@gmail.com>
2016-09-11 10:39:37 +0800

    added connection to mongoldb

--- api/server.js
+++ api/server.js
@@ -6,6 +6,13 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
+//Connect to mongodb
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/webgallery');
+
+//Models
+var User = require('./models/user');
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,9 +24,31 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+router.use(function(req, res, next){
+  console.log('API\'s call is comming');
+  next();
+});
+
+router.route('/users')
+  .post(function(req, res){
+    var user = new User();
+    user.email = req.body.email;
+    user.password = req.body.password;
+
+    user.save(function(err){
+      if (err)
+        res.send(err);
+      res.json({message: req.body.email + ' created!'});
+    });
+  })
+
+  .get(function(req, res){
+    res.json({message:"test"});
+  });
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
 // more routes for our API will happen here
@@ -32,4 +61,3 @@ app.use('/api', router);
 // =============================================================================
 app.listen(port);
 console.log('Server started on port ' + port);
-
