027acc1890589d4e4888dc8d68f43ef9c88b7f41
rahardiandj <rahardiandj@gmail.com>
2016-09-12 21:51:06 +0800

    Added token based authentication (prototype)

--- api/server.js
+++ api/server.js
@@ -26,12 +26,69 @@ app.use(morgan('dev'));
 
 var port = process.env.PORT || 8080;        // set our port
 
+
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
+app.use('/api', router);
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+router.route('/authenticate')
+  .post(function(req,res){
+    User.findOne({
+      email:req.body.email
+    },function(err, user){
+      if (err) throw err;
+
+      if (!user){
+        res.json({success:false, message:'Authentication Failed! Username not found'})
+
+      } else if (user){
+        if (user.password != req.body.password){
+          res.json({success:false, message: 'Authentication Failed! Password is incorrect'})
+        }else {
+          var token = jwt.sign(user, app.get('superSecret'), {
+          expiresIn: 60*60*24 // expires in 24 hours
+        });
+
+          res.json({
+            success: true,
+            message: 'Enjoy your token',
+            token: token
+          })
+        }
+      }
+    })
+  });
+
 
 router.use(function(req, res, next){
-  console.log('API\'s call is comming');
+  console.log(req.headers);
+  //check token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  if (token) {
+    jwt.verify(token, app.get('superSecret'),function(err,decoded){
+
+      if (err){
+        return res.json({success: false, message: 'Failed to Authenticate'})
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    })
+  } else {
+    return res.status(403).send({
+      success:false,
+      message:'No token provided'
+    });
+  }
+
   next();
 });
 
@@ -52,19 +109,18 @@ router.route('/users')
   })
 
   .get(function(req, res){
-    res.json({message:"test"});
+    User.find({}, function(err,users){
+      res.json(users);
+    });
   });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
+
 
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+
 
 // START THE SERVER
 // =============================================================================
